0cdbcf3d57a7c22f85ee613c058c6b55ce58fac9
JessV28 <158533245+JessV28@users.noreply.github.com>
2024-02-07 12:23:08 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const nodes = document.querySelectorAll('.node');
     const tooltip = document.createElement('div');
     tooltip.className = 'tooltip';
     document.body.appendChild(tooltip);
 
-    document.querySelectorAll('.node').forEach(node => {
-        // Display tooltip on hover
-        node.addEventListener('mouseover', (e) => {
-            const translation = node.getAttribute('data-translation');
-            if (translation) {
-                tooltip.textContent = translation;
-                tooltip.style.left = `${e.pageX + 10}px`; // Slight offset for better visibility
-                tooltip.style.top = `${e.pageY + 10}px`;
-                tooltip.classList.add('visible');
-            }
+    nodes.forEach(node => {
+        node.addEventListener('mouseover', function(e) {
+            const translation = this.getAttribute('data-translation');
+            tooltip.textContent = translation;
+            tooltip.style.left = `${e.pageX}px`;
+            tooltip.style.top = `${e.pageY}px`;
+            tooltip.classList.add('visible');
         });
 
-        node.addEventListener('mouseout', () => {
+        node.addEventListener('mouseout', function() {
             tooltip.classList.remove('visible');
         });
 
-        // Play audio on click
         node.addEventListener('click', function() {
             const audioFile = this.getAttribute('data-audio');
-            if (audioFile) {
-                const audio = new Audio(`audio/${audioFile}.mp3`);
-                audio.play();
+            const audio = new Audio(`audio/${audioFile}.mp3`);
+            audio.play();
+        });
+    });
+
+    const categories = document.querySelectorAll('.category');
+    categories.forEach(category => {
+        category.addEventListener('click', function() {
+            this.classList.toggle('active');
+            var content = this.nextElementSibling;
+            if (content.style.maxHeight) {
+                content.style.maxHeight = null;
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
             }
         });
     });
